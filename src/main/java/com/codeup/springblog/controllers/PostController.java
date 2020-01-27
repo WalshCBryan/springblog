1f2c2e619fb6586a198f8c519fbb3c7208ef1825
Bryan C Walsh <Walsh.C.Bryan@Gmail.com>
2020-01-27 10:42:24 -0600

    intro to spring controllers

---
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -0,0 +1,36 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public String postIndex(){
+      return "An index of all posts";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String post(@PathVariable int id){
+        return "Showing post id#: " + id;
+    }
+
+    @GetMapping("posts/create")
+    @ResponseBody
+    public String createNewPost(){
+        return "view form for creating posts";
+    }
+
+    @PostMapping("posts/create")
+    @ResponseBody
+    public String publishPost(){
+        return "Publishing new post";
+    }
+
+}
