0468cbf2fddd6a89ec366f03ce277923916673b1
Bryan C Walsh <Walsh.C.Bryan@Gmail.com>
2020-01-27 14:01:43 -0600

    first spring view

--- src/main/java/com/codeup/springblog/controllers/Deimos.java
+++ src/main/java/com/codeup/springblog/controllers/Deimos.java
@@ -1,6 +1,7 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,9 +9,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class Deimos {
 
+
     @GetMapping("/deimos/{days}")
-    @ResponseBody
-    public String howManyDays(@PathVariable int days){
-        return  days + " days till dev day!";
+    public String countdown(@PathVariable int days, Model model) {
+        model.addAttribute("days", days);
+        return "countdown";
     }
+
+//    @GetMapping("/deimos/{days}")
+//    @ResponseBody
+//    public String howManyDays(@PathVariable int days) {
+//        return days + " days till dev day!";
+//    }
 }
--- src/main/java/com/codeup/springblog/controllers/StringTransformController.java
+++ src/main/java/com/codeup/springblog/controllers/StringTransformController.java
@@ -67,3 +67,20 @@ public class StringTransformController {
     }
 
 }
+
+
+// Create a controller StringTransformController with the following methods
+// url = "/string/reverse/{string}"
+// returns = the string reversed
+// url = "/string/uppercase/{string}"
+// returns = string in all uppercase letters
+// url = "/string/both/string"
+// returns = string reversed in all caps (avoid repeating logic)
+// url = "/string/{string}" (with a query string)
+// returns = string reversed or in caps or both based on parameters passed to the Query string
+// (research how to capture Query string values in Spring MVC)
+// (for example... "/string/steve?reverse=true&caps=false" should return "evets")
+
+//3) Create a default message for any uri that is not mapped
+
+//4) Create a default message for any uri that starts with "/wildcard" and has any number of forward slashes and values after
